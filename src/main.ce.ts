41e3697a96f3270d23f1118367148ad3d1fbd0a6
yesworld <a.yesworld@gmail.com>
2023-12-04 12:26:17 +0300

    chore: delete useless files

--- src/main.ce.ts
+++
@@ -1,6 +0,0 @@
-import { defineCustomElement } from 'vue'
-import VueWebCustomElement from './VueWebCustomElement.vue'
-
-const webCustomElement = defineCustomElement(VueWebCustomElement)
-
-customElements.define('vue-web-custom-element', webCustomElement)
--- src/main.ts
+++
@@ -1,5 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import VueWebCustomElement from './VueWebCustomElement.vue'
-
-createApp(VueWebCustomElement).mount('#app')
