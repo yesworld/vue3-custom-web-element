db3ad046cc79aeaee7cb573cec9017d5b38fe0e2
yesworld <a.yesworld@gmail.com>
2023-12-26 11:19:43 +0300

    build(jquery): inject Jquery for old projects

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import inject from '@rollup/plugin-inject'
 
 const command = process.env.npm_lifecycle_event.split(':')
 
@@ -13,6 +14,9 @@ const application = command[1]
 export default defineConfig({
   root: 'src/'+application,
   plugins: [
+    inject({
+      $: 'jquery',
+    }),
     vue(
       {
       template: {
@@ -25,11 +29,21 @@ export default defineConfig({
   ],
   build: {
     outDir: resolve(__dirname, `./dist/${application}`),
-    // minify: 'esbuild',
     lib: {
       entry: `./main.ce.ts`,
       name: 'vue-web-custom-element',
       fileName: 'vue-web-custom-element'
+    },
+    sourcemap: true,
+    target: 'esnext',
+    minify: false,
+    rollupOptions: {
+      // external: ['vue'],
+      // output: {
+      //   globals: {
+      //     vue: 'Vue',
+      //   }
+      // }
     },
   },
   define: {
